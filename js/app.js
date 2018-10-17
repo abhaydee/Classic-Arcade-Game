122fe38ab5fdcb9a37fc08a78c4dc066f2dab22e
abhaydee <abhaygirish1997@gmail.com>
2018-10-17 12:22:15 -0700

    functionality of the game

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 // Enemies our player must avoid
 
-var Enemy = function(x,y) {
+var Enemy = function(x,y,speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
       this.x=x;
       this.y=y;
+
+      this.speed=speed;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -13,10 +15,28 @@ var Enemy = function(x,y) {
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    this.x+=1;
+    this.x+=1;this.speed;
+    //point1=this.x;
+    this.x+=this.speed*dt;
     if(this.x > 400){
         this.x=-100;
+        this.speed=50 + Math.floor(Math.random() * 350);
+       // console.log("the speed is set");
     }
+
+    let temp=player.x-this.x;
+    console.log(player.y);
+
+    console.log(this.y);
+    if((player.x<this.x +60) && (player.x +38>this.x) &&(player.y===(this.y))) {
+        player.x=200;
+        player.y=380;
+    }
+    
+
+   
+   
+
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
@@ -36,8 +56,9 @@ Enemy.prototype.render = function() {
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 var Player =function(x,y){
-    this.x=y;
+    this.x=x;
     this.y=y;
+    point2=this.y;
     this.playerimage='images/char-boy.png';
 
 }
@@ -57,26 +78,33 @@ Player.prototype.update=function(keypress){
         this.y=380;
     }
     //else if(this.x )
-    
+    //
+    //if(point1===point2){
+       // console.log("they are matched !Good work now,reset the game");
+    //   this.x=200;
+     //  this.y=380;
+        
+    //}
+   
        
 }
 
 Player.prototype.handleInput=function(keypress){
     if(keypress==="left"){
         this.x=this.x -100;
-        console.log(this.y);
+        //console.log(this.y);
     }
     else if(keypress==="right"){
         this.x=this.x + 100;
-        console.log(this.y);
+        //console.log(this.y);
     }
     else if(keypress==="up"){
-          this.y=this.y - 100 ;
-          console.log(this.y);
+          this.y=this.y - 80 ;
+         // console.log(this.y);
     }
     else if(keypress==="down"){
-        this.y=this.y + 100;
-        console.log(this.y);
+        this.y=this.y + 80;
+        //console.log(this.y);
     }
     else{
          window.alert("invalid key pressed!. Use the arrow Keys");
@@ -104,13 +132,15 @@ document.addEventListener('keyup', function(e) {
     player.handleInput(allowedKeys[e.keyCode]);
 });
 var allEnemies=[];
-var player= new Player(250,400);
-var enemyspeed=[200,300,400];
+var player= new Player(200,380);
+var enemyspeed=[200,280,360];
 var enemy;
+var point1;
+var point2;
 
 enemyspeed.forEach(function(e){
-     enemy=new Enemy(0,420);
-     enemy=new Enemy(0,420-e);
+     
+     enemy=new Enemy(0,420-e,100);
      
     allEnemies.push(enemy);
 })
